fc01afef0c234cb5f31362466c9554d7ade3ce4f
Alexandria White <114960634+alexandriawhite@users.noreply.github.com>
2023-03-23 21:08:29 -0500

    Updating PetList to server side. Trenton and I are still working on this

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,26 +2,52 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type User {
-    _id: ID
-    username: String
-    email: String
-    password: String
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    pets: [Pet!]
   }
 
-  type Auth {
-    token: ID!
-    user: User
+  type Pet {
+    _id: ID
+    name: String
+    species: String
+    breed: String
+    gender: String
+    age: String
+    weight: String
+    allergies: String
+    medications: String
+    feedingSchedule: String
+    owner: User
   }
 
   type Query {
-    users: [User]
-    user(username: String!): User
-    me: User
+    users: [User!]!
+    user(id: ID!): User
+    pets: [Pet!]!
+    pet(id: ID!): Pet
   }
 
   type Mutation {
-    addUser(username: String!, email: String!, password: String!): Auth
-    login(email: String!, password: String!): Auth  
+    createUser(username: String!, email: String!, password: String!): User!
+    createPet(name: String!, species: String!, breed: String!, age: String!, weight: String!, allergies: String, medications: String, feedingSchedule: String, ownerId: ID!): Pet!
+    updateUser(id: ID!, username: String, email: String, password: String): User
+    updatePet(id: ID!, name: String, species: String, breed: String, gender: String, age: String, weight: String, allergies: String, medications: String, feedingSchedule: String, ownerId: ID): Pet
+    deleteUser(id: ID!): User
+    deletePet(id: ID!): Pet
+    addPet(
+      name: String!,
+      species: String!,
+      breed: String!,
+      gender: String!,
+      age: String!,
+      weight: String!,
+      allergies: String,
+      medications: String,
+      feedingSchedule: String
+    ): Pet
   }
 `;
 
