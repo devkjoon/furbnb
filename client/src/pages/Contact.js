ba88589e16c1e964cbfb806fb53228413e833d3c
devkjoon <devkjoon@gmail.com>
2023-03-27 10:54:33 -0400

    added basic styling to contact component

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -3,10 +3,10 @@ import React from "react";
 
 export default function ContactPage() {
   return (
-    <div>
-      <h1>Contact Us</h1>
+    <div className="contact-container">
+      <h1 className="contact-title">Contact Us</h1>
       <p>Stay in touch with us on social media:</p>
-      <ul>
+      <ul className="contact-social">
         <li>
           <a href="https://www.facebook.com/yourpetdaycare/">Facebook</a>
         </li>
@@ -24,11 +24,13 @@ export default function ContactPage() {
         </li>
       </ul>
       <p>Or contact us directly:</p>
+      <div className="contact-info">
       <ul>
         <li>Phone: 555-555-5555</li>
-        <li>Email: info@yourpetdaycare.com</li>
+        <li>Email: <a href="mailto:info@yourpetdaycare.com">info@yourpetdaycare.com</a></li>
         <li>Address: 123 Main St, Anytown USA</li>
       </ul>
+      </div>
     </div>
   );
 }
