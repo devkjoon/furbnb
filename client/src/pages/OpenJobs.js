177150f83501fe1f7cd4ef0eb84d47352fbddbc7
Liam Allen <liamallen343@gmail.com>
2023-04-03 19:43:18 -0500

    Added styling to open jobs page

--- client/src/pages/OpenJobs.js
+++ client/src/pages/OpenJobs.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 function OpenJobs() {
   return (
-    <div>
-  <h1>Open Jobs</h1>
-  <ul>
-    <li>
+    <div className="jobs-cont">
+  <h1>Open <span className="blue-span">Jobs</span></h1>
+  <ul className="jobs-list">
+    <li className="job-entry">
       <h2>Dog Walker</h2>
       <p>We are looking for a responsible and reliable dog walker to join our team. The ideal candidate will have experience working with dogs of all sizes and breeds, and will be able to provide references from previous clients.</p>
-      <p>Requirements:</p>
+      <h5>Requirements:</h5>
       <ul>
         <li>Valid driver's license and reliable transportation</li>
         <li>Availability to work mornings, afternoons, and weekends</li>
@@ -16,10 +16,10 @@ function OpenJobs() {
         <li>Excellent communication skills</li>
       </ul>
     </li>
-    <li>
+    <li className="job-entry">
       <h2>Day Care Attendant</h2>
       <p>We are seeking a friendly and energetic day care attendant to join our team. The ideal candidate will have experience working with dogs and will be able to provide references from previous clients.</p>
-      <p>Requirements:</p>
+      <h5>Requirements:</h5>
       <ul>
         <li>Availability to work mornings, afternoons, and weekends</li>
         <li>Ability to supervise multiple dogs at once</li>
@@ -27,10 +27,10 @@ function OpenJobs() {
         <li>Basic knowledge of dog behavior and training techniques</li>
       </ul>
     </li>
-    <li>
+    <li className="job-entry">
       <h2>Front Desk Receptionist</h2>
       <p>We are looking for a reliable and organized front desk receptionist to join our team. The ideal candidate will have excellent customer service skills and will be able to handle a fast-paced work environment.</p>
-      <p>Requirements:</p>
+      <h5>Requirements:</h5>
       <ul>
         <li>Excellent communication and organizational skills</li>
         <li>Availability to work mornings, afternoons, and weekends</li>
@@ -38,10 +38,10 @@ function OpenJobs() {
         <li>Basic computer skills</li>
       </ul>
     </li>
-    <li>
+    <li className="job-entry">
       <h2>Pet Resort Cleaner</h2>
       <p>We are seeking a reliable and detail-oriented cleaner to join our pet resort team. The ideal candidate will have experience in cleaning and organizing, and will be able to work independently.</p>
-      <p>Requirements:</p>
+      <h5>Requirements:</h5>
       <ul>
         <li>Availability to work mornings and afternoons</li>
         <li>Ability to lift and move heavy objects</li>
