ffcd1bf20e82eb42e07c909151a78dfadb6d5923
CoulsonP <coulsonp@icloud.com>
2023-04-04 15:21:22 -0500

    chnaged schedule

--- client/src/pages/Schedule.js
+++ client/src/pages/Schedule.js
@@ -22,18 +22,20 @@ function Schedule() {
     if (error) return <p>Error :(</p>;
 
     return (
-        <div>
-            {data.bookings.map((booking) => (
-                <div key={booking._id}>
-                    <h3>{booking.pet}</h3>
-                    <p>Service Type: {booking.serviceType}</p>
-                    <p>Start Time: {new Date(booking.startTime).toLocaleString('en-US', options)}</p>
-                    <p>End Time: {new Date(booking.endTime).toLocaleString('en-US', options)}</p>
-                    {booking.notes && <p>Notes: {booking.notes}</p>}
-                </div>
-            ))}
-        </div>
+      <div className="schedule-container">
+        {data.bookings.map((booking) => (
+          <div key={booking._id}>
+            <h2 className='sched-h2'>Your Pets Schedule</h2>
+            <h3 className='sched-h3'>{booking.pet}</h3>
+            <p className="sched-p">Service Type: {booking.serviceType}</p>
+            <p className="sched-p">Start Time: {new Date(booking.startTime).toLocaleString('en-US', options)}</p>
+            <p className="sched-p">End Time: {new Date(booking.endTime).toLocaleString('en-US', options)}</p>
+            {booking.notes && <p className="text notes">Notes: {booking.notes}</p>}
+          </div>
+        ))}
+      </div>
     );
+    
 }
 
 export default Schedule;
