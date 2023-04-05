9398d0795163c0a19c4c784ee36266523d089ea3
Alexandria White <114960634+alexandriawhite@users.noreply.github.com>
2023-04-04 19:55:41 -0500

    ID is displaying instead of name but at least the dropdown works

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -26,52 +26,41 @@ const typeDefs = gql`
     image: String
     owner: User
   }
+
+  input PetInput {
+    _id: ID!
+    name: String!
+  }
+
   input BookingInput {
-    pet: String
+    pet: PetInput!
     serviceType: String!
     date: String!
     startTime: String!
     endTime: String!
     notes: String
   }
-  
+
   type Booking {
     _id: ID!
     user: User!
-    pet: String
+    pet: Pet!
     serviceType: String!
     date: String!
     startTime: String!
     endTime: String!
     notes: String
+    petInfo: Pet
   }
   
+
   type Mutation {
     createBooking(input: BookingInput!): Booking!
     updateBooking(id: ID!, input: BookingInput!): Booking!
     deleteBooking(id: ID!): Booking!
-  }
-  
-  
-  type Auth {
-    token: ID
-    user: User
-  }
-
-  type Query {
-    users: [User!]!
-    user(id: ID!): User
-    pets: [Pet]! 
-    pet(id: ID!): Pet
-    bookings: [Booking]
-  }  
-
-  type Mutation {
     addUser(firstName: String, lastName: String, email: String, password: String, address: String, phoneNumber: String): Auth
-
     login(email: String!, password: String!): Auth
     createPet(name: String!, species: String!, breed: String!, age: String!, weight: String!, allergies: String, medications: String, feedingSchedule: String, ownerId: ID!): Pet!
-
     updateUser(id: ID!, firstName: String, lastName: String, email: String, password: String): User
     updatePet(id: ID!, name: String, species: String, breed: String, gender: String, age: String, weight: String, allergies: String, medications: String, feedingSchedule: String, ownerId: ID): Pet
     deleteUser(id: ID!): User
@@ -88,6 +77,19 @@ const typeDefs = gql`
       feedingSchedule: String,
       image: String
     ): Pet
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Query {
+    users: [User!]!
+    user(id: ID!): User
+    pets: [Pet]!
+    pet(id: ID!): Pet
+    bookings: [Booking]
   }
 `;
 
